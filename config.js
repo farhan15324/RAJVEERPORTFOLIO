c592cffaa350e8828d876aad588a9ae455281a06
farhan15324 <bemohdfarhansiddquis@gmail.com>
2025-12-26 22:58:30 +0530

    config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
     // URL of the Google Sheet published as CSV
     // File -> Share -> Publish to web -> Select "Projects" sheet -> CSV
-    SHEET_CSV_URL: 'YOUR_GOOGLE_SHEET_CSV_URL_HERE',
+    SHEET_CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTnIQwF2_TYCZ6W6ijITt-rrzPe2iphj6SWXUMPPFYb4zSSc7AnnUIj7W0imnMYNKkGp91X5fu4bFlp/pub?gid=0&single=true&output=csv',
     
     // YouTube Data API Key v3
-    YOUTUBE_API_KEY: 'YOUR_YOUTUBE_API_KEY_HERE',
+    YOUTUBE_API_KEY: 'AIzaSyAyoliUoBqCTVIzrcKfUpjnMYNW5lKLEbM',
     
     // Optional: Admin Panel URL if you want a direct link from the site (hidden or footer)
     ADMIN_URL: ''
